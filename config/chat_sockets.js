45389d4471a0eb81faa970b5f13be3683485953c
ayush2342 <ayushsharma2342@gmail.com>
2023-05-19 13:20:55 +0530

    Chat Engine created

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,4 +1,37 @@
 
 module.exports.chatSockets = function(socketServer){
 
+    let io = require('socket.io')(socketServer, {
+        cors: {
+          origin: 'http://localhost:8000',
+          methods: ['GET', 'POST'],
+          allowedHeaders: ['Content-Type'],
+          credentials: true,
+        },
+      });
+    
+
+    io.sockets.on('connection',function(socket)
+    {
+        console.log('New connection received',socket.id)
+
+        socket.on('disconnect',function(socket)
+        {
+            console.log('Socket disconnected')
+        })
+
+        socket.on('join_room',function(data)
+        {
+            console.log('Joining Request Received...',data)
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined',data);
+        })
+
+        socket.on('send_message',function(data)
+        {
+            io.in(data.chatroom).emit('received_message',data);
+        })
+    })
 }
