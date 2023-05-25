9b66e6c8d4539579d48575869e0771dc7f8653ba
ayush2342 <ayushsharma2342@gmail.com>
2023-05-25 12:15:22 +0530

    Socket URL changed from Localhost

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -4,7 +4,7 @@ class chatEngine{
         this.chatBoxId=$(`#${chatBoxId}`);
         this.userEmail=userEmail;
 
-        this.socket=io.connect('http://localhost:5000')
+        this.socket=io.connect('http://3.83.128.21:5000')
 
         if(this.userEmail)
         {
